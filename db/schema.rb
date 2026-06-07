0ee48885315c9ae82452294d36be915b75896e66
Yuukago <yuka.ago.work@gmail.com>
2026-06-07 18:41:52 +0900

    著者名を追加した

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_06_04_014933) do
+ActiveRecord::Schema[8.0].define(version: 2026_06_07_085738) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
   end
 end
